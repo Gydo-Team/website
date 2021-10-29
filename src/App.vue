5c818e2dad4b5df672d8f175322b6d58bb56ee8e
loldonut <ricricramisogaming18@gmail.com>
2021-10-29 21:55:11 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Main from './components/Main.vue';
+
+</script>
+
+<script lang="ts">
+  export default {
+    name: 'App',
+    components: {
+      Main,
+    },
+  }
+</script>
+<template>
+  <body>
+  </body>
+  <Main msg="gydo.js" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #131313;
+  margin-top: 60px;
+}
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+export default {
+    name: 'Main',
+
+}
+</script>
+
+<template>
+    <header>
+        <h1>smh</h1>
+    </header>
+</template>
+
+<style scoped>
+body {
+    color: #131313;
+}
+</style>
